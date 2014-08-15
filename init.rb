10650f4bdeec26e998a808283fc63dad951eb3e4
yuki <yuki.1090@gmail.com>
2014-08-15 11:03:03 +0900

    create plugin & edit init.rb

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_list_simple_customize do
+  name 'Redmine List Simple Customize plugin'
+  author 'miz'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'https://github.com/miz33/redmine_list_simple_customize'
+  author_url 'https://github.com/miz33'
+end
